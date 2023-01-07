3b1e20f5ac2d2ea3b61e4d5b0eaa7f2e65a763bb
Oluwafemi Oloyede <oloyedeoluwafemi22@gmail.com>
2023-01-07 09:22:54 +0100

    getStaticProps

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react'
 
@@ -6,6 +7,8 @@ const coffeStore = () => {
   return (
     <div>
       coffee sore page {Router.query.id}
+      <Link href='/'>Back to home</Link>
+      <Link href='/coffee-store/dynamic'>Go to page dynamic</Link>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,20 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner'
+import Card from '../components/card'
+import coffeeStoresData from '../data/coffee-stores.json'
 
 const inter = Inter({ subsets: ['latin'] })
+export async function getStaticProps(context) {
+  return {
+    props: {
+      coffeeStores:coffeeStoresData,
+    }, // will be passed to the page component as props
+  }
+}
 
-export default function Home() {
+export default function Home(props) {
+  
   const handleOnBannerBtnClick = () => {
     console.log('hi bruce banner')
   }
@@ -24,8 +34,19 @@ export default function Home() {
       <main className={styles.main}>
       <Banner buttonText='View stores nearby' handleOnClick={handleOnBannerBtnClick}/>
       <div className={styles.heroImage}>
-      <Image src='/static/hero-image.png' width={700} height={400}/>
+      <Image src='/static/hero-image.png' alt='hero' width={700} height={400}/>
       </div>
+      {props.coffeeStores.length > 0 &&
+      <>
+       <h2 className={styles.heading2}>Toronto stores</h2>
+      <div className={styles.cardLayout}>
+      {props.coffeeStores.map(coffeeStore => {
+      return <Card 
+      key={coffeeStore.id}
+      name={coffeeStore.name} imgUrl={coffeeStore.imgUrl} href={`/coffee-store/${coffeeStore.id}`} className={styles.card}/>})}
+      </div>
+      </>}
+      
       </main>
     </div>
   )
